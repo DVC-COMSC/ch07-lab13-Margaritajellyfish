8b96816d386ab19cc849e7e43ffc2dbc05a31e32
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-17 04:29:01 +0000

    7lab13

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 rnum = len(numbers)
 cnum = len(numbers[0])
-
+count = 0
 # ******************************
 # Make your Code
 # ******************************
+
+for i in range(1,rnum-1):
+    for j in range(1,cnum-1):
+        if (numbers[i][j-1] + numbers[i][j] + numbers[i][j+1]) == 3:
+            if( numbers[i-1][j] + numbers[i+1][j]) == 2:
+                count +=1
+print(count)
